7221f6af2cc9c5f3e8695b410f8a9035dd9a3445
X4eger <114403787+X4eger@users.noreply.github.com>
2022-10-17 16:05:13 +0100

    End of Homework

--- week_03/day_01/Homework_script.sql
+++ week_03/day_01/Homework_script.sql
@@ -91,24 +91,149 @@ FROM employees
 ORDER BY salary DESC NULLS LAST 
 LIMIT 10;
 
+/* Q10 */
 
+-- Find the first_name, last_name and salary of the 
+-- lowest paid employee in Hungary.
+
+SELECT first_name,
+       last_name,
+       salary
+FROM employees
+WHERE country = 'Hungary'
+ORDER BY salary DESC NULLS LAST 
+LIMIT 1;
      
+ /* Q11 */
+
+-- How many employees have a first_name beginning with ‘F’?
+
+SELECT count(first_name)
+FROM employees 
+WHERE first_name 
+    LIKE 'F%'
      
+/* Q12 */
+    
+-- Find all the details of any employees with a ‘yahoo’ email address?
+    
+SELECT *
+FROM employees 
+WHERE email 
+    LIKE '%yahoo%'
+    
+/* Q13 */
+    
+-- Count the number of pension enrolled employees not based in 
+-- either France or Germany.
      
+SELECT count(*)
+FROM employees 
+WHERE  country NOT IN ('France', 'Germany')
+AND pension_enrol IS TRUE
      
+/* Q14 */
+
+-- What is the maximum salary among those employees in 
+-- the ‘Engineering’ department who work 1.0 full-time 
+-- equivalent hours (fte_hours)?
+
+SELECT max(salary) AS max_fulltime_engineering_salary
+FROM employees 
+WHERE department = 'Engineering'
+AND fte_hours = 1.0
      
+/* Q15 */
+
+-- Return a table containing each employees 
+-- first_name, last_name, full-time equivalent hours (fte_hours), 
+-- salary, and a new column effective_yearly_salary which should 
+-- contain fte_hours multiplied by salary.
      
+SELECT first_name,
+       last_name,
+       fte_hours,
+       salary,
+       (fte_hours * salary) AS effective_yearly_salary
+FROM employees 
+WHERE fte_hours IS NOT NULL 
+AND salary IS NOT NULL
+
+/* Extension */
+
+/* Q16 */
+
+-- The corporation wants to make name badges for a 
+-- forthcoming conference. Return a column badge_label 
+-- showing employees’ first_name and last_name joined together 
+-- with their department in the following style: ‘Bob Smith - Legal’. 
+-- Restrict output to only those employees with stored first_name, 
+-- last_name and department.
+
+
+SELECT 
+    first_name || ' ' || last_name || ' - ' || department AS badge_label
+FROM employees
+WHERE  (first_name IS NOT NULL
+    AND  last_name IS NOT NULL
+    AND  department IS NOT NULL)
+
+/* Q17 */
+    
+-- One of the conference organisers thinks it would be 
+-- nice to add the year of the employees’ start_date to 
+-- the badge_label to celebrate long-standing colleagues, 
+-- in the following style ‘Bob Smith - Legal (joined 1998)’. 
+-- Further restrict output to only those employees with a stored start_date.
+
+-- [If you’re really keen - try adding the month as a string: 
+-- ‘Bob Smith - Legal (joined July 1998)’] 
+ 
+    
+SELECT 
+    first_name || ' ' || 
+    last_name || ' - ' || 
+    department || ' (joined ' ||
+    TO_CHAR(start_date, 'FMMonth, YYYY')
+    ||    ')'
+AS badge_label
+FROM employees
+WHERE  
+    (first_name IS NOT NULL
+    AND  last_name IS NOT NULL
+    AND  department IS NOT NULL
+    AND start_date IS NOT NULL)
+    
      
-     
-     
-     
-     
-     
-     
-     
-     
-     
+/* Q18 */
+    
+-- Return the first_name, last_name and salary 
+-- of all employees together with a new column 
+-- called salary_class with a value 'low' where 
+-- salary is less than 40,000 and value 'high' where 
+-- salary is greater than or equal to 40,000. 
+
+    
+SELECT first_name,
+       last_name,
+       salary,
+       CASE
+           WHEN salary < 40000 THEN 'Low'
+           WHEN salary >= 40000 THEN 'High'
+           ELSE 'No Salary Data'
+       END
+       AS salary_class
+FROM employees
       
+
+
+
+
+
+
+
+
+
       
       
       
